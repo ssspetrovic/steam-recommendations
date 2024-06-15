1b0498422442c0a720d5a2ece8dec6caab956279
Srdjan Petrovic <s.petrovic852@gmail.com>
2024-06-15 02:30:52 +0200

    fix aggr

--- srdjan/src/aggregations_final.js
+++ srdjan/src/aggregations_final.js
@@ -5,75 +5,77 @@
       from: "Games",
       localField: "review_id",
       foreignField: "app_id",
-      as: "game_reviews",
-    },
+      as: "game_reviews"
+    }
   },
   {
-    $unwind: "$game_reviews",
+    $unwind: "$game_reviews"
   },
   {
     $lookup: {
       from: "Platform",
       localField: "game_reviews.app_id",
       foreignField: "app_id",
-      as: "game_platform",
-    },
+      as: "game_platform"
+    }
   },
   {
-    $unwind: "$game_platform",
+    $unwind: "$game_platform"
   },
   {
     $match: {
       "game_reviews.positive_ratio": { $gte: 80 },
       "game_platform.steam_deck": true,
-      funny: { $gt: 0 },
-    },
+      funny: { $gt: 0 }
+    }
   },
   {
     $project: {
       review_id: 1,
       funny: 1,
       "game_reviews.title": 1,
-      "game_reviews.positive_ratio": 1,
-    },
+      "game_reviews.positive_ratio": 1
+    }
   },
   {
-    $limit: 5,
-  },
-][
-  // maksimalan broj sati koje su korisnici igrali igre za koje nisu preporučili, grupišući ih po igrama.
-  ({
+    $limit: 5
+  }
+]
+
+// maksimalan broj sati koje su korisnici igrali igre za koje nisu preporučili, grupišući ih po igrama.
+[
+  {
     $lookup: {
       from: "Recommendations",
       localField: "app_id",
       foreignField: "app_id",
-      as: "recommendations",
-    },
+      as: "recommendations"
+    }
   },
   {
-    $unwind: "$recommendations",
+    $unwind: "$recommendations"
   },
   {
     $match: {
-      "recommendations.is_recommended": false,
-    },
+      "recommendations.is_recommended": false
+    }
   },
   {
     $group: {
       _id: {
         game_id: "$app_id",
-        game_name: "$title",
+        game_name: "$title"
       },
       max_hours_played: {
-        $max: "$recommendations.hours",
+        $max: "$recommendations.hours"
       },
       users_with_max_hours: {
         $push: {
           user_id: "$recommendations.user_id",
-          hours: "$recommendations.hours",
-        },
-      },
-    },
+          hours: "$recommendations.hours"
+        }
+      }
+    }
   },
   {
     $project: {
@@ -85,86 +87,93 @@
         $arrayElemAt: [
           "$users_with_max_hours.user_id",
           {
-            $indexOfArray: ["$users_with_max_hours.hours", "$max_hours_played"],
-          },
-        ],
-      },
-    },
+            $indexOfArray: [
+              "$users_with_max_hours.hours",
+              "$max_hours_played"
+            ]
+          }
+        ]
+      }
+    }
   },
   {
     $sort: {
-      max_hours_played: -1,
-    },
+      max_hours_played: -1
+    }
   },
   {
-    $limit: 5,
-  })
-][
-  // najveći fanovi "Dota 2" igre na osnovu maksimalnog broja sati koje su igrali.
-  ({
+    $limit: 5
+  }
+]
+
+// najveći fanovi "Dota 2" igre na osnovu maksimalnog broja sati koje su igrali.
+[
+  {
     $match: {
       hours: { $gt: 20 },
       is_recommended: true,
-      user_id: { $mod: [10, 2] },
-    },
+      user_id: { $mod: [10, 2] }
+    }
   },
   {
     $lookup: {
       from: "Games",
       localField: "app_id",
       foreignField: "app_id",
-      as: "game_info",
-    },
+      as: "game_info"
+    }
   },
   {
-    $unwind: "$game_info",
+    $unwind: "$game_info"
   },
   {
     $match: {
-      "game_info.title": "Dota 2",
-    },
+      "game_info.title": "Dota 2"
+    }
   },
   {
     $group: {
       _id: "$user_id",
-      max_hours_played: { $max: "$hours" },
-    },
+      max_hours_played: { $max: "$hours" }
+    }
   },
   {
     $sort: {
-      max_hours_played: -1,
-    },
+      max_hours_played: -1
+    }
   },
   {
-    $limit: 10,
+    $limit: 10
   },
   {
     $project: {
       _id: 0,
       user_id: "$_id",
-      max_hours_played: 1,
-    },
-  })
-][
-  // procenat igara koje imaju vrlo pozitivan rejting, uz prosečnu cenu i prosečne korisničke recenzije.
-  ({
+      max_hours_played: 1
+    }
+  }
+]
+
+// procenat igara koje imaju vrlo pozitivan rejting, uz prosečnu cenu i prosečne korisničke recenzije.
+[
+  {
     $lookup: {
       from: "Price",
       localField: "app_id",
       foreignField: "app_id",
-      as: "price_info",
-    },
+      as: "price_info"
+    }
   },
   {
-    $unwind: "$price_info",
+    $unwind: "$price_info"
   },
   {
     $group: {
       _id: "$rating",
       count: { $sum: 1 },
       average_price: { $avg: "$price_info.price_final" },
-      average_reviews: { $avg: "$user_reviews" },
-    },
+      average_reviews: { $avg: "$user_reviews" }
+    }
   },
   {
     $group: {
@@ -175,54 +184,59 @@
           rating: "$_id",
           count: "$count",
           average_price: "$average_price",
-          average_reviews: "$average_reviews",
-        },
-      },
-    },
+          average_reviews: "$average_reviews"
+        }
+      }
+    }
   },
   {
-    $unwind: "$ratings",
+    $unwind: "$ratings"
   },
   {
     $project: {
       _id: 0,
       rating: "$ratings.rating",
       percentage: {
-        $multiply: [{ $divide: ["$ratings.count", "$total"] }, 100],
+        $multiply: [
+          { $divide: ["$ratings.count", "$total"] },
+          100
+        ]
       },
       average_price: "$ratings.average_price",
-      average_reviews: "$ratings.average_reviews",
-    },
+      average_reviews: "$ratings.average_reviews"
+    }
   },
   {
     $match: {
-      rating: "Very Positive",
-    },
-  })
-][
-  // najpopularnije igre na platformama Mac i Linux sa cenom manjom od 15 dolara, sortirane po broju korisničkih recenzija.
-  ({
+      rating: "Very Positive"
+    }
+  }
+]
+
+// najpopularnije igre na platformama Mac i Linux sa cenom manjom od 15 dolara, sortirane po broju korisničkih recenzija.
+[
+  {
     $lookup: {
       from: "Platform",
       localField: "app_id",
       foreignField: "app_id",
-      as: "platform",
-    },
+      as: "platform"
+    }
   },
   {
     $lookup: {
       from: "Price",
       localField: "app_id",
       foreignField: "app_id",
-      as: "price",
-    },
+      as: "price"
+    }
   },
   {
     $match: {
       "platform.mac": true,
       "platform.linux": true,
-      "price.price_final": { $lt: 15 },
-    },
+      "price.price_final": { $lt: 15 }
+    }
   },
   {
     $project: {
@@ -230,15 +244,15 @@
       app_id: 1,
       title: 1,
       user_reviews: 1,
-      price_final: { $arrayElemAt: ["$price.price_final", 0] },
-    },
+      price_final: { $arrayElemAt: ["$price.price_final", 0] }
+    }
   },
   {
     $sort: {
-      user_reviews: -1,
-    },
+      user_reviews: -1
+    }
   },
   {
-    $limit: 10,
-  })
-];
+    $limit: 10
+  }
+]
